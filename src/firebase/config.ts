e8ea1613fa1da61a99d0766f3caddbdce520186a
Edson <edsoncantuariaa@gmail.com>
2026-02-21 04:19:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-837139650-f35ff",
+  "appId": "1:683643000342:web:fd43225aec82ea47c0c737",
+  "apiKey": "AIzaSyDbVW1ZnGXa7mDDf5xzp1gbZiP3jf4YHE0",
+  "authDomain": "studio-837139650-f35ff.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "683643000342"
+};
